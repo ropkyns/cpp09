a4c596d1ce58ce8ef2885eaee696f5a2e9117327
ropkyns <ropkyns@gmail.com>
2025-06-13 15:48:19 +0200

    ex02

---
+++ ex02/include/PmergeMe.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/13 11:02:02 by paulmart          #+#    #+#             */
+/*   Updated: 2025/06/13 14:35:24 by paulmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGME_HPP
+
+# include <vector>
+# include <deque>
+# include <fstream>
+# include <iostream>
+# include <sstream>
+# include <cstdlib>
+# include <cerrno>
+# include <cctype>
+
+
+
+#endif
---
+++ ex02/src/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/13 11:01:46 by paulmart          #+#    #+#             */
+/*   Updated: 2025/06/13 15:47:52 by paulmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/PmergeMe.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 3)
+		std::cerr << "Error : not enough argument" << std::endl;
+	else
+	{
+		;
+	}
+}
